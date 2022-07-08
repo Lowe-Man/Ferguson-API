b7bee1c5d2456a50e8296e0efcfbf99604747267
Alex Lowe <loweman64@outlook.com>
2022-07-07 21:40:22 -0400

    chore: Add Cors Headers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const studentRoutes = require('./routes/student');
 const errorHandler = require("./util/error");
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 
 app.use('/student', studentRoutes);
 
